505161d16306159d2c6902c53954052d29ba771a
anshuljain <anshul09.jain@gmail.com>
2017-07-14 02:59:02 -0400

    working example of spring web mvc

--- MyProjects/src/com/spring/code/controller/HelloWorldController.java
+++ MyProjects/src/com/spring/code/controller/HelloWorldController.java
@@ -1,5 +1,16 @@
 package com.spring.code.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class HelloWorldController {
 
+	@RequestMapping(path="/hello",method=RequestMethod.GET)
+	public String printHelloMessage(ModelMap map){
+		map.addAttribute("message","This is coming from HelloWorldController");
+		return "hello";
+	}
 }
