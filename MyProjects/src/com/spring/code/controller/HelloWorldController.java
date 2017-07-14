7fe6c04d5252dd9037abfdef09131eb2eec44321
axj49 <anshuljain@equifax.com>
2017-07-14 16:14:53 -0400

    added helloWorldService class

--- MyProjects/src/com/spring/code/controller/HelloWorldController.java
+++ MyProjects/src/com/spring/code/controller/HelloWorldController.java
@@ -1,16 +1,23 @@
 package com.spring.code.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.code.service.HelloWorldService;
 
 @Controller
 public class HelloWorldController {
 
-	@RequestMapping(path="/hello",method=RequestMethod.GET)
-	public String printHelloMessage(ModelMap map){
-		map.addAttribute("message","This is coming from HelloWorldController");
+	@Autowired
+	public HelloWorldService helloWorldService;
+	
+	@RequestMapping(value="/hello/{firstName}", method=RequestMethod.GET)
+	public String printHelloMessage(ModelMap map, @PathVariable("firstName") String fName){
+		map.addAttribute("message","This is updated from HelloWorldController. The first name is "+helloWorldService.convertToUpperCase(fName));
 		return "hello";
 	}
 }
