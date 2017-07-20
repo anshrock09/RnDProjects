1fdd99e826b7016037c80c6484a9a6ec0ba531f4
Anshul Jain <anshul09.jain@gmail.com>
2017-07-19 21:20:37 -0400

    added use of @RequestParam

--- MyProjects/src/com/spring/code/controller/HelloWorldController.java
+++ MyProjects/src/com/spring/code/controller/HelloWorldController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.code.configuration.HelloWorldBeanConfig;
 import com.spring.code.service.HelloWorldService;
@@ -25,11 +26,11 @@ public class HelloWorldController {
 	}*/
 	
 	@RequestMapping(value="/hello/{firstName}", method=RequestMethod.GET)
-	public String printHelloMessage(ModelMap map, @PathVariable("firstName") String name){
+	public String printHelloMessage(ModelMap map, @PathVariable("firstName") String name, @RequestParam("lastName") String lastName){
 		
 		
 		map.addAttribute("message","This is the message from HelloWorldController.<br/>The string parameter in UPPERCASE is "+helloWorldServic.convertToUpperCase(name)+""
-				+ "<br/>The string parameter in LOWERCASE is "+helloWorldBeanConfig.getMessageFromHelloWorldService(name));
+				+ "<br/>The request parameter in LOWERCASE is "+helloWorldBeanConfig.getMessageFromHelloWorldService(lastName));
 		return "hello";
 	}
 }
